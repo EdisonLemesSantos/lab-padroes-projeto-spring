f78fb24bccaa13bd5a8b69073e348849b271b403
EdisonLemesSantos <edisonlemessantos@gmail.com>
2022-11-14 15:12:59 -0300

    Inicializando repositorio

---
+++ src/main/java/one/digitalinnovation/gof/Service/ClienteService.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.gof.Service;
+
+import one.digitalinnovation.gof.Model.Cliente;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ * 
+ * @author Edison Lemes dos Santos
+ */
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
